6ab6cbe211cb491e94f89679a007e80dd60db3bb
your name <jackwillee1995@gmail.com>
2020-06-14 10:29:27 +0200

    added test to check for title on views

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get help" do
     get pages_help_url
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "about"
   end
 
 end

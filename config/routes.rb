2565b9fca0c88640efe20c15e9588c533e23312d
your name <jackwillee1995@gmail.com>
2020-06-18 21:39:58 +0200

    changed routes into named routes and each button apart from sign up goes to the correct page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-root 'pages#home' 
-get 'help' => 'pages#help' 
-get 'about' => 'pages#about' 
-get 'contact' => 'pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get  '/help',    to: 'pages#help'
+  get  '/about',   to: 'pages#about'
+  get  '/contact', to: 'pages#contact'
 end

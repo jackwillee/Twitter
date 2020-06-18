89e6d6640d55ac7d10ffdc404cbd6228075823b6
your name <jackwillee1995@gmail.com>
2020-06-18 20:15:42 +0200

    contact page set up and test written to make sure it works

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home"
   end
 
   test "should get help" do
-    get pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help"
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "about"
   end
@@ -24,4 +24,10 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact"
+  end
 end

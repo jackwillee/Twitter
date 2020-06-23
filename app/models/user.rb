7cfa40a83b856ac138e5ff324fdd885b684d90c6
your name <jackwillee1995@gmail.com>
2020-06-23 17:10:38 +0200

    implemented validations and fixed errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    validates :name,  presence: true, length: { maximum: 40 } #makes sure name is present in record with a maximum char count of 40
-    validates :email, presence: true, length: { maximum: 255 }#makes sure email is present in record with a maximum char count of 255
+    validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,9 +18,17 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?
     end
     test "name should not have too many characters" do 
-    @user.name = 'a' * 50
+    @user.name = 'a' * 51
     assert_not @user.valid?
       
+    end
+    
+    test "email addresses should be unique" do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      assert_not duplicate_user.valid?
+      assert_not duplicate_user.valid?
     end
 end 
     

4870030384b6270f213e556c98eba8d4e8fe754c
your name <jackwillee1995@gmail.com>
2020-06-24 15:33:10 +0200

    updated user.rb model with validations to make sure password is required and is set to a minimum length of 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: 'secret')
+    @user = User.new(name: "Example User", email: "user@example.com", password: 'secret', password_confirmation: 'secret')
   end
 
   test "should be valid" do
@@ -28,6 +28,19 @@ class UserTest < ActiveSupport::TestCase
       @user.save
       assert_not duplicate_user.valid?
     end
+    test "password should be present (nonblank)" do
+      @user.password = @user.password_confirmation = " " * 6
+      assert_not @user.valid?
+    end
+  
+    test "password should have a minimum length" do
+      @user.password = @user.password_confirmation = "a" * 5
+      assert_not @user.valid?
+    end
+
+
+      
+    
 end 
     
   
